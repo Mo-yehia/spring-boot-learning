04b6ae04bbaabc0dedfbaefd98f748dab84ac9c6
Mohamed Yehia <122978317+Mo-yehia@users.noreply.github.com>
2025-07-09 13:24:30 +0300

    Add files via upload

---
+++ beanScopes&LifeCycle/SingletonVsPrototype/src/main/java/org/beanScopesAndLifeCycle/myBeans/myBean.java
@@ -0,0 +1,10 @@
+package org.beanScopesAndLifeCycle.myBeans;
+
+public class myBean {
+
+    // Tracks the number of times myBean instances are created.
+    public myBean() {
+        System.out.println("my bean is created!");
+    }
+
+}
